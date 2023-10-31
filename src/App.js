c335a611c99f4d50d36241985977b3285732ec68
Mohammad Naqaweh <mohammad.naqaweh@gmail.com>
2023-10-31 23:53:32 +0300

    Represent Data From API Call using JobPost component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import { JobPost } from './components';
 
 function App() {
+  const [jobPosts , setJobPosts] = useState([]);
+  
+  console.log(jobPosts);
+
+  const getJobPosts = async () => {
+    try{
+      const res = await axios.get("http://localhost:4000/");
+      setJobPosts(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getJobPosts();
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='jobPosts__table'>
+      {jobPosts.map((item, index) => (
+        <JobPost 
+          key={index}
+          item={item}
+        />
+      ))}
     </div>
   );
 }
