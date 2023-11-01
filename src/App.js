72d00a20d0198943c6a703229f8cd26a9a69e2bd
Mohammad Naqaweh <mohammad.naqaweh@gmail.com>
2023-11-02 02:26:30 +0300

    Refactor Api Calls to sperate file to make code easier to read and maintain

--- src/App.js
+++ src/App.js
@@ -1,45 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import './App.css';
 import { JobPost } from './components';
-import {getCountries} from './api/APIUtils.js'
+import {getJobPosts ,getCoordinates} from './api/APIUtils.js'
 import MapContainer from './components/MapContainer/MapContainer';
 
 function App() {
   const [jobPosts , setJobPosts] = useState([]);
   const [coordinates ,setCoordinates] = useState([]);
 
-  const getJobPosts = async () => {
-    try{
-      const res = await axios.get("http://localhost:4000/jobPosts");
-      setJobPosts(res.data);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  const getCoordinates = async (jobPosts) => {
-    const c = [];
-    const countries = getCountries(jobPosts);
-    countries.forEach(async (country) => {
-      try {
-        const res = await axios.get(`http://localhost:4000/countryCoordinates`, {
-          params: {country: country}
-        })
-        c.push(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    })
-    setCoordinates(c);
-  }
-
   useEffect(() => {
-    getJobPosts();
+    getJobPosts(setJobPosts);
   },[])
   
   useEffect(() => {
-    getCoordinates(jobPosts);
+    getCoordinates(jobPosts,setCoordinates);
+    console.log(coordinates);
   },[jobPosts])
 
   return (
--- src/api/APIUtils.js
+++ src/api/APIUtils.js
@@ -1,4 +1,6 @@
-export const getCountries = (jobPosts) => {
+import axios from 'axios';
+
+const getCountries = (jobPosts) => {
   let countries = [];
   jobPosts.forEach((item) =>{
     const country = item.item.country;
@@ -7,4 +9,29 @@ export const getCountries = (jobPosts) => {
     }
   })
   return countries;
+}
+
+export const getJobPosts = async (setJobPosts) => {
+  try{
+    const res = await axios.get("http://localhost:4000/jobPosts");
+    setJobPosts(res.data);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export const getCoordinates = async (jobPosts ,setCoordinates) => {
+  const c = [];
+  const countries = getCountries(jobPosts);
+  countries.forEach(async (country) => {
+    try {
+      const res = await axios.get(`http://localhost:4000/countryCoordinates`, {
+        params: {country: country}
+      })
+      c.push(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  setCoordinates(c);
 }
