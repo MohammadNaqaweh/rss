18aeebb279ae829394c3ce823e6b0bc53de78804
Mohammad Naqaweh <mohammad.naqaweh@gmail.com>
2023-11-02 17:57:24 +0300

    MapPopUp Component shows all jobPosts at as react-bootstrap card component

--- src/components/MapPopUp/MapPopUp.jsx
+++ src/components/MapPopUp/MapPopUp.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { redirectToPost } from '../../utils/utils.js'
 
-const MapPopUp = () => {
+const MapPopUp = ( {jobPosts} ) => {
+  
+
   return (
     <div>
-      
+      {jobPosts.map((job, index) => (
+        <Card key={index} style={{width: '300px'}}>
+          <Card.Body>
+            <Card.Title>{job.title}</Card.Title>
+            <Card.Text>{job.posted_date}</Card.Text>
+            <Button onClick ={() => {redirectToPost(job.link)}} variant='primary'>Apply</Button>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
+
   )
 }
 
