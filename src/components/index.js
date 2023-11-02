d1f412cfe1843c0877dbbb549961ffb19eea3ebd
Mohammad Naqaweh <mohammad.naqaweh@gmail.com>
2023-11-02 22:31:33 +0300

    Remove unused Title Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { default as JobPost } from './JobPost/JobPost';
 export { default as PopUp } from './PopUp/PopUp';
-export { default as Title } from './Title/Title';
 export { default as JobInfo } from './JobInfo/JobInfo';
 export { default as MapPopUp } from './MapPopUp/MapPopUp';
 export { default as InfoWindowWrapper } from './InfoWindowWrapper/InfoWindowWrapper';
