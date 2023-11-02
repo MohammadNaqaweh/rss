fdeaa6c54a71634cc1282f849df1c3a5ea56c5a0
Mohammad Naqaweh <mohammad.naqaweh@gmail.com>
2023-11-02 22:29:44 +0300

    Update PopUp Component to use react-bootstrap components

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import parse from 'html-react-parser';
-import { Title,JobInfo } from '../index.js';
+import { Button, Card } from 'react-bootstrap';
 import { redirectToPost } from '../../utils/utils.js'
 import './popup.css';
 
 const PopUp = ({ title,country,postedDate,description, link }) => {
   return ( 
-    <>
-      <Title title={title}/>
-      <JobInfo country={country} postedDate={postedDate}/>
-      <div className='jobPosts__table-post_description'>{parse(description)}</div>
-      <div className='jobPosts__table-post_button'>
-      <button onClick={() => redirectToPost(link)}>Apply</button>
-      </div>
-    </>
+    <Card>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>
+          {country} {postedDate}
+          <div className='jobPosts__table-post_description'>{parse(description)}</div>
+        </Card.Text>
+        <Button onClick={() => redirectToPost(link)}>Apply</Button>
+      </Card.Body>
+    </Card>
   )
 }
 
