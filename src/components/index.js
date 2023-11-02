6b69c8fd09bf194263406074544adf4871cd5814
Mohammad Naqaweh <mohammad.naqaweh@gmail.com>
2023-11-02 22:32:46 +0300

    Remove unused JobInfo component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export { default as JobPost } from './JobPost/JobPost';
 export { default as PopUp } from './PopUp/PopUp';
-export { default as JobInfo } from './JobInfo/JobInfo';
 export { default as MapPopUp } from './MapPopUp/MapPopUp';
 export { default as InfoWindowWrapper } from './InfoWindowWrapper/InfoWindowWrapper';
 export { default as JobPosts } from './JobPosts/JobPosts';
