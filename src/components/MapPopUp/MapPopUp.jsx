2968a7a700a29c180402eb5e5023a9c6a928f663
Mohammad Naqaweh <mohammad.naqaweh@gmail.com>
2023-11-02 16:47:41 +0300

    MapPopUp React Component

---
+++ src/components/MapPopUp/MapPopUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MapPopUp = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MapPopUp
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as JobPost } from './JobPost/JobPost';
 export { default as PopUp } from './PopUp/PopUp';
 export { default as Title } from './Title/Title';
 export { default as JobInfo } from './JobInfo/JobInfo';
+export { default as MapPopUp } from './MapPopUp/MapPopUp'
