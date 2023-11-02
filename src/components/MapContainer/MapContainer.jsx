7be2b01148d32435fb0b5983be31b150d3aaec02
Mohammad Naqaweh <mohammad.naqaweh@gmail.com>
2023-11-02 16:13:59 +0300

    added markers to the map based on job coordinates and Added InfoWindows to show jobs in that location onclick to the marker at that location

--- src/components/MapContainer/MapContainer.jsx
+++ src/components/MapContainer/MapContainer.jsx
@@ -1,7 +1,31 @@
 import React, {Component} from 'react'
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {getJobPostsByCountry} from '../../api/APIUtils.js'
 
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+    jobs: []
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+  });
+ 
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
+
   render() {
     const style = {
       // width: '300px',
@@ -22,13 +46,25 @@ export class MapContainer extends Component {
         scrollwheel={false}
         mapTypeControl={false}
         streetViewControl={false}
+        onClick={this.onMapClicked}
       >
         {this.props.coordinates.map((c,i) => (
-          <Marker 
+          <Marker
             key={i}
+            name={c.country}
             position={{lat: c.lat, lng: c.lng}}
+            onClick={this.onMarkerClick}
           />
         ))}
+
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        >
+          {getJobPostsByCountry(this.props.jobPosts, this.state.selectedPlace.name).map((job, i)=>(
+            <p>{job.title}</p>
+          ))}
+        </InfoWindow>
 
       </Map>
     );
